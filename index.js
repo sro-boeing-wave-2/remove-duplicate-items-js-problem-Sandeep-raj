4757792498fde365a6b9603bc2265a4092d240db
Sandeep <gurusandeepraj@gmail.com>
2018-07-13 03:46:18 +0530

    program is reduced to set object constructor

--- index.js
+++ index.js
@@ -4,14 +4,7 @@
  * @returns {Array}
  */
 
-const s = new Set();
-function Add(element) {
-  s.add(element);
-}
-const removeDuplicateItems = (items) => {
-  items.forEach(Add);
-  const arr = Array.from(s);
-  return arr;
-};
+
+const removeDuplicateItems = items => Array.from(new Set(items));
 
 module.exports = removeDuplicateItems;
